ed43f7d95110c5e152a2ce04238822171e4f1b21
vedhaduj <97368023+vedhaduj@users.noreply.github.com>
2022-05-28 11:00:53 -0400

    Update DaysUntilXmas.py

--- Holidays/DaysUntilXmas.py
+++ Holidays/DaysUntilXmas.py
@@ -1,1 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
 
+url = "https://www.calendarr.com/united-states/how-many-days/christmas-day/"
+
+response = requests.get(url).text
+soup = BeautifulSoup(response, 'lxml')
+
+xmas = soup.find('div', class_ = 'day-counter--detail-text').text.replace('Now that you know how many days are left until   Christmas Day, share it with your friends.', ' ').replace('   ',' ').replace('Day!', '')
+print(xmas)
+input()
